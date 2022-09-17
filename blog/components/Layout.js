57f24ef07e82bcf36e1a0ec1499485db1b994982
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-17 17:28:58 +0900

    fix: add margin to the single post view

--- blog/components/Layout.js
+++ blog/components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
           <a className='text-right hover:text-blue-700'>About Me</a>
         </Link>
       </header>
-      <main className='container mx-auto flex-1'>{children}</main>
+      <main className='container mx-auto p-2 flex-1'>{children}</main>
       <footer className='bg-indigo-100 mt-8 py-4 text-slate-800'>
         <div className='container mx-auto flex justify-center'>
           &copy; 2022 Kurumi Muto
