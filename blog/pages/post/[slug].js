d3d605401d8c0e207af6507d77cf85b0469888ed
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-17 18:41:39 +0900

    fix: handle null tags

--- blog/pages/post/[slug].js
+++ blog/pages/post/[slug].js
@@ -31,7 +31,7 @@ export default function PostPage({ frontmatter, content }) {
     <div className='prose mx-auto'>
       <h1>{frontmatter.title}</h1>
       <span>{frontmatter.date}</span>
-      {frontmatter.tags.map(tag => {
+      {frontmatter.tags && frontmatter.tags.map(tag => {
         return <button className='rounded bg-indigo-100 hover:bg-indigo-200 px-1 mx-2'>{tag}</button>
       })}
       <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
