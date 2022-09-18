1f3447c9ee8d0c920c3bda70a9fbdc2dbaa2369d
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-18 21:37:02 +0900

    feat: add ogp tags

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,9 +1,20 @@
 import Layout from "../components/Layout";
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Kurumiのブログ</title>
+        <meta property="og:site_name" content="Kurumiのブログ" />
+        <meta property="og:description" content="非情報系の女子学生ががんばってプログラミングをするブログ" />
+        <meta property="og:image" content="https://walnut07.com/images/image.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        { pageProps.content && <meta property="og:type" content="article" /> }
+        { pageProps.frontmatter && 
+          <meta property="og:description" content={`${pageProps.frontmatter.title} - 非情報系の女子学生ががんばってプログラミングをするブログ`} /> }
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
