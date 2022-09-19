4fc74cc7bf1b7851f02a2c96b7b733ac7bdbfbfd
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-19 21:20:00 +0900

    fix: edit ogp tags

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -8,17 +8,19 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Kurumiのブログ</title>
         <meta property="og:site_name" content="Kurumiのブログ" />
-        <meta property="og:title" content="Kurumiのブログ" />
         <meta property="og:description" content="非情報系の女子学生ががんばってプログラミングをするブログ" />
         <meta property="og:image" content="https://walnut07.com/images/image.png" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="og:type" content="website" />
         
-        { pageProps.content && <meta property="og:type" content="article" /> }
-        { pageProps.frontmatter && 
+        { pageProps.frontmatter ?
           <>
           <meta property="og:type" content="article" />
           <meta property="og:title" content={`${pageProps.frontmatter.title}`} />
+          </>
+          :
+          <>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Kurumiのブログ" />
           </>
         }
       </Head>
