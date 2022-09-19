57d8aa3991b7f1aae24798f65cca368bfde293c6
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-19 17:59:23 +0900

    chore: add another ogp tag for the main page

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Kurumiのブログ</title>
         <meta property="og:site_name" content="Kurumiのブログ" />
+        <meta property="og:title" content="メインページ" />
         <meta property="og:description" content="非情報系の女子学生ががんばってプログラミングをするブログ" />
         <meta property="og:image" content="https://walnut07.com/images/image.png" />
         <meta property="twitter:card" content="summary_large_image" />
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }) {
         { pageProps.content && <meta property="og:type" content="article" /> }
         { pageProps.frontmatter && 
           <>
+          <meta property="og:type" content="article" />
           <meta property="og:title" content={`${pageProps.frontmatter.title}`} />
           </>
         }
