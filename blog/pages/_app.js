74756d6607c1de098b7f93a1f5168f943cc0739d
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-09-19 21:02:33 +0900

    fix: add ogp type

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -8,10 +8,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Kurumiのブログ</title>
         <meta property="og:site_name" content="Kurumiのブログ" />
-        <meta property="og:title" content="メインページ" />
+        <meta property="og:title" content="main" />
         <meta property="og:description" content="非情報系の女子学生ががんばってプログラミングをするブログ" />
         <meta property="og:image" content="https://walnut07.com/images/image.png" />
         <meta property="twitter:card" content="summary_large_image" />
+        <meta property="og:type" content="main" />
         
         { pageProps.content && <meta property="og:type" content="article" /> }
         { pageProps.frontmatter && 
