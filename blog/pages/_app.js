40695cc49f550962322cf0948178da6324fd9009
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-10-02 20:41:16 +0900

    style: comment out code G Analytics

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -3,22 +3,29 @@ import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // TODO: Add Google Analytics
+  // window.dataLayer = window.dataLayer || [];
+  // function gtag(){dataLayer.push(arguments);}
+  // gtag('js', new Date());
+
+  // gtag('config', 'G-LDCEPYFYN6');
   return (
     <Layout>
       <Head>
         <title>Kurumiのブログ</title>
-        <meta property="og:site_name" content="Kurumiのブログ" />
         <meta property="og:description" content="非情報系の女子学生ががんばってプログラミングをするブログ" />
         <meta property="og:image" content="https://walnut07.com/images/image.png" />
         <meta property="twitter:card" content="summary_large_image" />
         
         { pageProps.frontmatter ?
           <>
+          <meta property="og:site_name" content={`Kurumiのブログ - ${pageProps.frontmatter.title}`} />
           <meta property="og:type" content="article" />
           <meta property="og:title" content={`${pageProps.frontmatter.title}`} />
           </>
           :
           <>
+          <meta property="og:site_name" content="Kurumiのブログ" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Kurumiのブログ" />
           </>
